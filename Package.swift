cf5a2d972b6fb67fc1d12e1160714a4e3465718c
Gil Biren <gilbiren@gmail.com>
2020-11-28 17:40:29 +0100

    Exclude example

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BenchmarkWrapper",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Example"]),
         .testTarget(
             name: "BenchmarkWrapperTests",
             dependencies: ["BenchmarkWrapper"]),
