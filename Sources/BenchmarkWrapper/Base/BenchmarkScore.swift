dceafb462016551356992bc71c2d795a91291ef2
Gil Biren <gilbiren@gmail.com>
2020-11-30 14:55:48 +0100

    Add public init

--- Sources/BenchmarkWrapper/Base/BenchmarkScore.swift
+++ Sources/BenchmarkWrapper/Base/BenchmarkScore.swift
@@ -11,4 +11,9 @@ public struct BenchmarkScore: Identifiable {
     public let id = UUID()
     public let score: Int
     public let configuration: BenchmarkServiceConfigurationProtocol
+    
+    public init(score: Int, configuration: BenchmarkServiceConfigurationProtocol) {
+        self.score = score
+        self.configuration = configuration
+    }
 }
