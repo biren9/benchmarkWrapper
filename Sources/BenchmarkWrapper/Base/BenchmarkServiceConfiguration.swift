240bbf5917027b7dcda143b5dc937d72907e961c
Gil Biren <gilbiren@gmail.com>
2020-11-30 11:33:21 +0100

    Add public init

--- Sources/BenchmarkWrapper/Base/BenchmarkServiceConfiguration.swift
+++ Sources/BenchmarkWrapper/Base/BenchmarkServiceConfiguration.swift
@@ -13,4 +13,12 @@ public struct BenchmarkConfiguration: BenchmarkServiceConfigurationProtocol {
     public let description: String?
     public let cpuCoreRunType: CpuCoreRunType
     public let duration: TimeInterval
+    
+    public init(qualityOfService: QualityOfService, serviceType: BenchmarkServiceProtocol.Type, description: String? = nil, cpuCoreRunType: CpuCoreRunType, duration: TimeInterval) {
+        self.qualityOfService = qualityOfService
+        self.serviceType = serviceType
+        self.description = description
+        self.cpuCoreRunType = cpuCoreRunType
+        self.duration = duration
+    }
 }
