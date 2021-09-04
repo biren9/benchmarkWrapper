d82b3a04f1290fd0ad9bc2642954c27713569a17
Gil Biren <gilbiren@gmail.com>
2021-09-04 12:28:56 +0200

    Add tvOS

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "BenchmarkWrapper",
     platforms: [
         .macOS(.v10_15),
-        .iOS(.v14)
+        .iOS(.v14),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
